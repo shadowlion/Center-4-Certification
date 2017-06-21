fa7398ad796c90efa031330a81078592ef3aa769
Justin Chiou <chiou.kai@gmail.com>
2017-06-21 08:43:09 -0400

    Gut contents, start with the basics

---
+++ layouts/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container">
+        <p class="text-xs-center">&copy;
+            <?php echo date("Y"); ?>
+            <a href="mailto:Dr-Cert@center4certs.org">Center For Certification</a>
+        </p>
+    </div>
+</footer>
+
+<script src="assets/js/functions.js"></script>
+
+</body>
+</html>
