810e8e735127aa2ee097f46dd5870127cf92652f
Justin Chiou <chiou.kai@gmail.com>
2017-06-22 09:24:31 -0400

    init footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,12 +1,15 @@
-<footer>
+<footer class="footer">
     <div class="container">
-        <p class="text-xs-center">&copy;
-            <?php echo date("Y"); ?>
-            <a href="mailto:Dr-Cert@center4certs.org">Center For Certification</a>
+        <p class="text-muted">
+            Built and designed with <i class="fa fa-heart" aria-hidden="true"></i> by <a>Justin Chiou</a> and <a>Ari Buchwald</a>.
+            <br>&copy; <?php echo date("Y"); ?> Center for Certification
         </p>
     </div>
 </footer>
 
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 <script src="assets/js/functions.js"></script>
 
 </body>
