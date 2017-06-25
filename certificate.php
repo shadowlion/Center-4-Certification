0d1947dd266b7458e87473be5701e4fca0529139
Justin Chiou <chiou.kai@gmail.com>
2017-06-25 18:09:01 -0400

    finish 1st draft of certificate

--- certificate.php
+++ certificate.php
@@ -1,49 +1,17 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require_once 'templates/head.php';
-
+// include 'lib/emailAdmin.php';
+include 'layouts/head.php';
 ?>
 
 <body>
-
-<section id="menu-0">
-    <nav class="navbar navbar-dropdown bg-color transparent navbar-fixed-top">
-        <div class="container">
-            <div class="mbr-table">
-                <div class="mbr-table-cell">
-                    <div class="navbar-brand">
-                        <a href="https://center4certs.org" class="navbar-logo">
-                            <img src="assets/images/c4c-logo-2000x1241.png" alt="Center For Certification" title="Center For Certification">
-                        </a>
-                        <a class="navbar-caption" href="index.php"><font color="grey">Center For Certification</font></a>
-                    </div>
-			        <div float: right><br />
-            <button class="btn btn-primary" onClick="window.print()">Print your certificate</button>
+    <div class="certificateArea">
+        <img src="assets/images/C4C-<?php echo $_POST['certColor']; ?>.png">
+        <div class="certificateContent Klarissa">
+            <h3><?php echo date("F j, Y"); ?></h3>
+            <h2><?php echo $_POST['certTitle']; ?></h2>
+            <h1><?php echo  $_POST['fullName']; ?></h1>
         </div>
-                </div>
-            </div>
-        </div>
-    </nav>
-</section>
+    </div>
+</body>
 
-<section>
-        <div style="position: relative" width="1000" height="768" border="0" padding:"20px;">
-            <img src="assets/images/C4C-<?php echo $_POST['certColor']; ?>.png">
-        </div>
-        <div align="center" style="position: absolute; left: 301px; top: 210px; width: 418px; height: 429px;" width="1000" text-align: center; padding:20px>
-            <h3><font face="klarissa_contourregular"><?php echo date("F j, Y"); ?></font><br /></h3>
-        	<img src="assets/images/spacer.gif" width="415" height="106"><br>
-			<h2><font face="klarissa_contourregular"><?php echo  $_POST['certification']; ?></font><br /></h2>
-			<img src="assets/images/spacer.gif" width="413" height="98"><br>
-			<h1><font face="klarissa_contourregular"><?php echo  $_POST['fullName']; ?></font><br /></h1>
-        </div>
-</section>
-
-<?php
-
-include 'includes/emailAdmin.php';
-require_once 'templates/footer.php';
-
-?>
+<?php include 'layouts/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require 'layouts/head.php';
 include 'layouts/nav.php';
 
-$page = $_GET['page'];	/* gets the variable $page */
-	if (!empty($page)) {
-		$page .= '.php';
-		include($page);
-	} 	/* if $page has a value, include it */
-	else {
-		include('home.php');
-	} 	/* otherwise, include the default page */
+if (isset($_GET['page'])) {
+	$page = $_GET['page'];	/* gets the variable $page */
+	$page .= '.php';
+	include($page);
+} 	/* if $page has a value, include it */
+else {
+	include('home.php');
+} 	/* otherwise, include the default page */
 
 require 'layouts/footer.php';
 
